f806c97428c7aa84f906fd01210416aa28f0a9e2
[[ Marcos Esparza]] <112668379+mesparza0@users.noreply.github.com>
2026-05-29 18:22:27 -0500

    update

---
+++ Homework_1A/Program.cs
@@ -0,0 +1,37 @@
+﻿// Question 1 - 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Pay Rate: ");
+            double payRate = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter amount of Hours worked: ");
+            double hoursWorked = Convert.ToDouble(Console.ReadLine());
+
+            double grossPay = payRate * hoursWorked;
+            double taxes = grossPay * 0.0495;
+            double netPay = grossPay - taxes;
+
+            Console.WriteLine();
+            Console.WriteLine("Payroll Summary");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Employee Name: " + name);
+            Console.WriteLine("Gross Employee Pay: " + grossPay.ToString("C"));
+            Console.WriteLine("Taxes: " + taxes.ToString("C"));
+            Console.WriteLine("Net Employee Pay: " + netPay.ToString("C"));
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+            Console.WriteLine("Error details: " + ex.Message);
+        }
+    
+    }
+}
