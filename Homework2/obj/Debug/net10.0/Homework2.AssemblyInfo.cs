c51034681cd503cf3d54541c22c27eed9c9f82ce
[[ Marcos Esparza]] <112668379+mesparza0@users.noreply.github.com>
2026-05-29 19:04:26 -0500

    programming assigment

--- Homework2/obj/Debug/net10.0/Homework2.AssemblyInfo.cs
+++ Homework2/obj/Debug/net10.0/Homework2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c83a8fcd567ff18cc8e34f25e0039330bc6152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f806c97428c7aa84f906fd01210416aa28f0a9e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
